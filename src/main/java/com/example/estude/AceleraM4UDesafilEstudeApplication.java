6b2bd63e71150ced4cbc52e26b172dd44bc8051b
Vitor Maverick <vitor.maverick@acad.ifma.edu.br>
2021-10-27 23:58:09 -0300

    Criação do projeto Spring e configurações do banco

---
+++ src/main/java/com/example/estude/AceleraM4UDesafilEstudeApplication.java
@@ -0,0 +1,13 @@
+package com.example.estude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleraM4UDesafilEstudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceleraM4UDesafilEstudeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estude/AceleraM4UDesafilEstudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceleraM4UDesafilEstudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
