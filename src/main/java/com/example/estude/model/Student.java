d328aa3581c6e22803897424c5058504ffd9d38f
Vitor Maverick <vitor.maverick@acad.ifma.edu.br>
2021-10-28 02:09:06 -0300

    Mapeando a classe Student

---
+++ src/main/java/com/example/estude/model/Student.java
@@ -0,0 +1,62 @@
+package com.example.estude.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Integer idStudent;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Integer getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(Integer idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(idStudent, student.idStudent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idStudent);
+    }
+}
---
+++ src/main/java/com/example/estude/repository/IStudentRepository.java
@@ -0,0 +1,13 @@
+package com.example.estude.repository;
+
+import com.example.estude.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface IStudentRepository extends JpaRepository<Student, Integer> {
+
+    List<Student> findByNameContaining(String name);
+
+
+}
