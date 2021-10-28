6a581e560f85c21308a349a980566fd65b8e85dc
Vitor Maverick <vitor.maverick@acad.ifma.edu.br>
2021-10-28 03:30:13 -0300

    Mapeando a classe Student

--- src/main/java/com/example/estude/model/Student.java
+++ src/main/java/com/example/estude/model/Student.java
@@ -2,6 +2,7 @@ package com.example.estude.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
@@ -9,7 +10,7 @@ import java.util.Objects;
 public class Student {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer idStudent;
     private String name;
     private String email;
