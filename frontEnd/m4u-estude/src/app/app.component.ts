97c3605252b9749e084d3e8b49dfa83067577096
Valdinéia da Silva <82914359+vsilva-code@users.noreply.github.com>
2021-11-07 00:23:24 -0300

    inclusão das classes model

--- frontEnd/m4u-estude/src/app/app.component.ts
+++ frontEnd/m4u-estude/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'm4u-estude';
+
+  public form!: FormGroup;
+  
+
+
 }
